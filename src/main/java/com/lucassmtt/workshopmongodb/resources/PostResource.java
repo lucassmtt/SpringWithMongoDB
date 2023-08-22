fa0c27297c2ebc672825b096aa8b965531edb16f
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-08-22 13:42:41 -0300

    Creating search for id in posts...

---
+++ src/main/java/com/lucassmtt/workshopmongodb/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.lucassmtt.workshopmongodb.resources;
+
+import com.lucassmtt.workshopmongodb.domain.Post;
+import com.lucassmtt.workshopmongodb.services.PostService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ src/main/java/com/lucassmtt/workshopmongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package com.lucassmtt.workshopmongodb.services;
+
+import com.lucassmtt.workshopmongodb.domain.Post;
+import com.lucassmtt.workshopmongodb.repositories.PostRepository;
+import com.lucassmtt.workshopmongodb.services.exceptions.ObjectNotFoundException;
+import com.mongodb.BasicDBObject;
+import org.apache.logging.log4j.message.StringFormattedMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id){
+        if (repository.findById(id).isEmpty()){
+            throw new ObjectNotFoundException("Resouce not found");
+        }
+        return repository.findById(id).get();
+    }
+}

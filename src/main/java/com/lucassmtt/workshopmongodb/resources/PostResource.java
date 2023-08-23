dfc03b63a4bd18e416fb6bd98ca26f2d8fd58cc6
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-08-23 15:56:40 -0300

    Create search with query methods...

--- src/main/java/com/lucassmtt/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/lucassmtt/workshopmongodb/resources/PostResource.java
@@ -1,14 +1,14 @@
 package com.lucassmtt.workshopmongodb.resources;
 
 import com.lucassmtt.workshopmongodb.domain.Post;
+import com.lucassmtt.workshopmongodb.resources.util.URL;
 import com.lucassmtt.workshopmongodb.services.PostService;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -21,5 +21,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+       text = URL.decodeParam(text);
+       List<Post> posts = service.findByTitleContaining(text);
+       return ResponseEntity.ok().body(posts);
     }
 }
--- src/main/java/com/lucassmtt/workshopmongodb/services/PostService.java
+++ src/main/java/com/lucassmtt/workshopmongodb/services/PostService.java
@@ -22,4 +22,8 @@ public class PostService {
         }
         return repository.findById(id).get();
     }
+
+    public List<Post> findByTitleContaining(String text) {
+        return repository.findByTitleContaining(text);
+    }
 }
